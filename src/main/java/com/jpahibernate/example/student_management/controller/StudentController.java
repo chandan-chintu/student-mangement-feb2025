d19808a8cd522c27f88d462fe121d94adb149603
Chandan K M <chandanchintu16@gmail.com>
2025-02-24 21:55:40 +0530

    Implementing find, delete and update(put and patch both) APIs.

--- src/main/java/com/jpahibernate/example/student_management/controller/StudentController.java
+++ src/main/java/com/jpahibernate/example/student_management/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.jpahibernate.example.student_management.controller;
 import com.jpahibernate.example.student_management.model.Student;
 import com.jpahibernate.example.student_management.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student/apis")
@@ -20,4 +19,37 @@ public class StudentController {
         String response = studentService.addStudent(student);
         return response;
     }
+
+    @GetMapping("/find/{id}")
+    public Student findStudentById(@PathVariable int id){
+        Student student = studentService.findStudentById(id);
+        return student;
+    }
+
+    @GetMapping("/findAll")
+    public List<Student> findAllStudents(){
+        List<Student> studentList = studentService.findAllStudents();
+        return studentList;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudentById(@PathVariable int id){
+        String response = studentService.deleteStudentById(id);
+        return response;
+    }
+
+    @PutMapping("/updatePut/{id}")
+    public String updateStudentByPut(@PathVariable int id,@RequestBody Student studentRequest){
+        String response = studentService.updateStudentByPut(id, studentRequest);
+        return response;
+    }
+
+    //  @RequestParam  - it takes the input in the form of request parameter query
+    @PatchMapping("/updatePatch/{id}")
+    public String updateStudentMobileByPatch(@PathVariable int id, @RequestParam String mobile){
+        String response = studentService.updateStudentMobileByPatch(id,mobile);
+        return response;
+    }
+
+
 }
--- src/main/java/com/jpahibernate/example/student_management/service/StudentService.java
+++ src/main/java/com/jpahibernate/example/student_management/service/StudentService.java
@@ -5,6 +5,9 @@ import com.jpahibernate.example.student_management.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service // it contains business logic
 public class StudentService {
 
@@ -14,5 +17,49 @@ public class StudentService {
     public String addStudent(Student student){
         studentRepository.save(student);
         return "Student saved successfully";
+    }
+
+    public Student findStudentById(int id){
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        return studentOptional.get();
+    }
+
+    public List<Student> findAllStudents(){
+        List<Student> studentList = studentRepository.findAll();
+        return studentList;
+    }
+
+    public String deleteStudentById(int id){
+        studentRepository.deleteById(id);
+        return "Student with id : "+id+" is deleted successfully";
+    }
+
+    // updating whole object
+    public String updateStudentByPut(int studentId, Student studentRequest){
+        // find student with id
+        // if id present update student
+        // else cannot update
+        Student studentFromDb = findStudentById(studentId);
+        if(studentFromDb!=null){
+            studentRepository.save(studentRequest);
+            return "Student updated successfully";
+        } else{
+            return "Cannot find student to update";
+        }
+    }
+
+    // updating specific field
+    public String updateStudentMobileByPatch(int studentId, String mobile){
+        // find student with id
+        // if id present update student
+        // else cannot update
+        Student studentFromDb = findStudentById(studentId);
+        if(studentFromDb!=null){
+            studentFromDb.setMobile(mobile);
+            studentRepository.save(studentFromDb);
+            return "Student mobile number updated successfully";
+        } else{
+            return "Cannot find student to update";
+        }
     }
 }
